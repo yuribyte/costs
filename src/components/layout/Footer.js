7f9c044ba0e64ccbec16507418e0ed23b9449601
Yuri Vitor <yuri.lins@gft.com>
2022-04-13 19:35:12 -0300

    update Footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,6 +1,27 @@
+import styles from './Footer.module.css'
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
 
 function Footer() {
-  return <h1>Footer</h1>
+  const year = new Date().getFullYear()
+
+  return (
+    <div className={styles.footer}>
+      <ul className={styles.social_list}>
+        <li>
+          <FaFacebook />
+        </li>
+        <li>
+          <FaInstagram />
+        </li>
+        <li>
+          <FaLinkedin />
+        </li>
+      </ul>
+      <p className={styles.copy_right}>
+        <span>Costs</span> &copy; {year}
+      </p>
+    </div>
+  )
 }
 
 export default Footer
