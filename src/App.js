6690d525f5bfee2591a59a05f34cfc7d57ca88f2
Yuri Vitor <yuri.lins@gft.com>
2022-04-13 19:06:50 -0300

    add Container with workaround

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+
+// Pages
 import Home from './components/pages/Home'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
 import NotFound from './components/pages/NotFound'
+
+// Layout
+import Container from './components/layout/Container'
 
 function App() {
   return (
@@ -15,13 +20,15 @@ function App() {
         <Link to='/contact'>Contact</Link>
         <Link to='/newproject'>NewProject</Link>
       </ul>
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route exact path='/company' element={<Company />} />
-        <Route exact path='/contact' element={<Contact />} />
-        <Route exact path='/newproject' element={<NewProject />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Container customClass='min-height'>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/company' element={<Company />} />
+          <Route exact path='/contact' element={<Contact />} />
+          <Route exact path='/newproject' element={<NewProject />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Container>
       <p>Footer</p>
     </Router>
   );
